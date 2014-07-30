8c82d0dfb36536d4895a613c07cc8bf9b156bb1f
potpotpot <andor.tittel@doclerholding.com>
2014-07-30 17:53:52 +0200

    password helper test random generated text

--- test/RegistrationSystem/PasswordHelperTest.php
+++ test/RegistrationSystem/PasswordHelperTest.php
@@ -12,6 +12,9 @@ use Kata\RegistrationSystem\PasswordHelper;
 class PasswordHelperTest extends \PHPUnit_Framework_TestCase {
 
 
+    /**
+     *
+     */
     public function testGenerateRandomString()
     {
         $length = 12;
@@ -20,5 +23,53 @@ class PasswordHelperTest extends \PHPUnit_Framework_TestCase {
         $this->assertNotEmpty($randomString);
         $this->assertEquals($length, strlen($randomString));
     }
+
+    /**
+     *
+     */
+    public function testRandomStringCharacters()
+    {
+        $length = rand(6, 64);
+        $pwHelper = new PasswordHelper;
+        $randomString = $pwHelper->generateRandomString($length);
+        $isValidString = $this->isValidRandomString($randomString);
+        $this->assertTrue($isValidString);
+
+        // Adding an invalid character to the string and testing for false
+        $randomString .= '~';
+        $isValidString = $this->isValidRandomString($randomString);
+        $this->assertFalse($isValidString);
+    }
+
+    /**
+     * @param $randomString
+     * @return bool
+     */
+    public function isValidRandomString($randomString)
+    {
+        $isValidString = true;
+        if (preg_match('/[^a-zA-Z0-9+\/\*\-#$@%<>\(\)]/i', $randomString, $preg)) {
+            //var_export($preg);
+            $isValidString = false;
+        }
+        return $isValidString;
+    }
+
+    /**
+     *
+     */
+    public function testHashStringWithSalt()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function testGeneratePassword()
+    {
+
+    }
+
 }
  
