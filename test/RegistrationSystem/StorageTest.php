27344091937150b9ec46d204120198a276e1a89e
Kolos Kaszaly <kolos.kaszaly@escalion.com>
2014-08-06 18:51:14 +0200

    Registration system - pdo test

--- test/RegistrationSystem/StorageTest.php
+++ test/RegistrationSystem/StorageTest.php
@@ -17,79 +17,36 @@ class StorageTest extends \PHPUnit_Framework_TestCase {
     /**
      * @var \PDO
      */
-//    private $PDO;
-//    private $storage;
-//
-//    const DB_SCHEMA = "
-//        CREATE TABLE `users` (
-//          `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
-//          `email` varchar(256) default '',
-//          `password` text default '',
-//          `salt` text default '',
-//          KEY (`email`)
-//        )
-//    ";
-//
-//    protected function setUp() {
-//        $this->PDO = new \PDO('sqlite::memory:');
-//        $this->PDO->query(self::DB_SCHEMA);
-//        echo "setup ok\n";
-//        //$this->storage = new Storage($this->PDO);
-//    }
-//
-//    public function testSaveUser() {
-//        $user = new User();
-//        $user->email = 'test@example.com';
-//        $user->password = (new PasswordHelper())->generatePassword('testPlainPassword');
-//
-//       // $this->storage->saveUser($user);
-//
-//        $tmp = count($this->PDO->query("SELECT 1 as 'test' FROM users")->fetchAll());
-//        var_export($tmp);
-//        //$this->assertEquals(1, $this->PDO->query("SELECT id FROM users WHERE email='test@example.com'")->fetchAll());
-//    }
-
-    static private $pdo = null;
+    private $PDO;
+    private $storage;
 
     const DB_SCHEMA = "
-
+        CREATE TABLE users (
+          id INTEGER AUTO_INCREMENT PRIMARY KEY,
+          email varchar(256) default '',
+          password text default '',
+          salt text default ''
+        )
     ";
 
-    private function getConnection()
-    {
-        if (self::$pdo == null) {
-            self::$pdo = new \PDO('sqlite::memory:');
-        }
-        var_export(self::$pdo);
-        return self::$pdo;
+    protected function setUp() {
+        $this->PDO = new \PDO('sqlite::memory:');
+        $this->PDO->query(self::DB_SCHEMA);
+        echo "setup ok\n";
+        $this->storage = new Storage($this->PDO);
     }
 
-    public function setUp() {
-        try
-        {
-            $db = new \PDO('sqlite::memory:');
-            echo "SQLite created in memory.";
-            $db->exec('CREATE TABLE test (id INTEGER)');
-            $x = $db->query("show tables");
-            var_export($x->fetchAll());
-            exit;
-        }
-        catch(PDOException $e)
-        {
-            echo $e->getMessage();
-        }
-        exit;
+    public function testSaveUser() {
+        $user = new User();
+        $user->email = 'test@example.com';
+        $user->password = (new PasswordHelper())->generatePassword('testPlainPassword');
 
-        //$res->fetchAll();
-        $x = $res->fetchAll();
-        echo "setup\n";
-        exit;
+        $this->storage->saveUser($user);
+
+
+        $this->assertEquals(1, count($this->PDO->query("SELECT id FROM users WHERE email='test@example.com'")->fetchAll()));
     }
 
-    public function testValami() {
 
-        var_export($x);
-        echo 1;
-    }
 }
  
